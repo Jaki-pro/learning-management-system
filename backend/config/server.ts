3aecf9ef024f9627d45e0b536a62e7bbb296e53e
Jakaria Hossain <mdjak8980@gmail.com>
2025-10-06 17:37:42 +0600

    fix:deployment issue

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,6 +1,11 @@
-export default ({ env }) => ({
+// path: ./config/server.js
+
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL'),
+  // We use a dedicated boolean ENV variable to enable the proxy.
+  proxy: env.bool('PROXY_ENABLED', false),
   app: {
     keys: env.array('APP_KEYS'),
   },
