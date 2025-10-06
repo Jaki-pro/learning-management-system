f426898f51da25aed704c52aa7c19ebd5360fa36
jaki-pro <mdjak8980@gmail.com>
2025-10-06 13:58:17 +0600

    fix:deployment issue

--- backend/config/admin.ts
+++ backend/config/admin.ts
@@ -1,20 +1,11 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
+  sessions: {
+    enabled: true,
+    secure: false, // 👈 disables secure flag for cookies
+    sameSite: 'none', // optional but safer behind proxy
   },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
-    },
-  },
-  secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
-  },
+  url: '/admin',
 });
--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,8 +1,7 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', process.env.PORT || 3000),
-  proxy: true,
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+module.exports = ({ env }) => ({
+  host: '0.0.0.0',
+  port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://learning-management-system-production-2179.up.railway.app'),
+  proxy: true, // 👈 important: trust Railway proxy
+  app: { keys: env.array('APP_KEYS') },
 });
