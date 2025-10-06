20e4a9aa7ee1e2ff0e20a61ff4f347130431c07a
jaki-pro <mdjak8980@gmail.com>
2025-10-06 12:33:55 +0600

    chore: deploy strapi in railway

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', process.env.PORT || 3000),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
